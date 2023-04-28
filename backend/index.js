b019649155d52b00ab806df7fc73147229169022
sainRaghuveer <raghuveersain987@gmail.com>
2023-04-28 17:40:28 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,14 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+
+app.get((req,res)=>{
+    res.send("Welcome to Todo app backend!");
+});
+
+
+app.listen(8800, ()=>{
+    console.log("App is running at port 8800");
+})
