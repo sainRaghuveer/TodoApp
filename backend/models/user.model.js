2cc9183c5bec1369a26c978f0561133b5415ae7f
sainRaghuveer <raghuveersain987@gmail.com>
2023-05-08 22:25:59 +0530

    todo

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    time:Date
+});
+
+const userModel= new mongoose.model("User", userSchema);
+
+module.exports={
+    userModel
+}
